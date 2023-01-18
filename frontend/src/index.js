b6d2ba3b006fd4151afe23bbee96d9b08d9e6527
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-18 14:43:33 +0000

    installing chakra-ui and display first button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap ChakraProvider at the root of your app */}
+    <ChakraProvider>
+      {/* <TheRestOfYourApplication /> */}
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
