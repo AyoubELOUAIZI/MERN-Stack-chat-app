7aa0889ac3c847f634458c100346aa528f6c9304
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-20 04:33:24 +0000

    creat authentication form with chakra ui

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
-export const Homepage = () => {
+function Homepage() {
+  // use the useHistory hook to access the browser history
+  const history = useHistory();
+
+  useEffect(() => {
+    // check if there is a user in local storage
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    // if there is a user, redirect to the /chats route
+    if (user) history.push("/chats");
+  }, [history]);
+
   return (
-    <div>Homepage</div>
-  )
+    <Container maxW="xl" centerContent >
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+
+            <TabPanel>
+              {/* render login form component */}
+              <Login />
+            </TabPanel>
+
+            <TabPanel>
+              {/* render signup form component */}
+              <Signup />
+            </TabPanel>
+
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
+
 export default Homepage;
